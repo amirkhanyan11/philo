7dc9c0f814eb63fb18c112cfa14a5c11d0b39064
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-06-23 20:57:40 +0400

    .

--- src/__exit.c
+++ src/__exit.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 18:02:29 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/06/23 20:46:08 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/06/23 20:56:37 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 void __attribute__((noreturn)) __exit(char const * const err)
 {
 	if (NULL != err)
+	{
+		printf(RED);
 		printf("./philo: %s\n", err);
+		printf(RESET);
+	}
 	exit (EXIT_FAILURE);
 }
