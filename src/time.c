b4f17e7f92eb12bbf0501bb34dce51059ea0a39d
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-06-29 17:16:03 +0400

    .

--- src/time.c
+++ src/time.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 18:58:31 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/06/26 20:43:07 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/06/29 17:15:59 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,55 +26,6 @@ t_value	get_time(t_time_code time_code)
 		__exit("Bad time", __scary_error_info__);
 	return (-1);
 }
-
-// void	ft_usleep(long usec, t_table *table)
-// {
-// 	long	asleep;
-// 	long	spent;
-// 	long	left;
-
-// 	spent = 0;
-// 	asleep = ft_gettm(USEC);
-// 	while (spent < usec)
-// 	{
-// 		if (dinner_finished(table) == true)
-// 			break ;
-// 		spent = ft_gettm(USEC) - asleep;
-// 		left = usec - spent;
-// 		if (left > 1000)
-// 			usleep(left / 2);
-// 		else
-// 		{
-// 			while (ft_gettm(USEC) - asleep < usec)
-// 				;
-// 		}
-// 	}
-// }
-
-
-// void	ft_usleep(t_value sec, t_table *table)
-// {
-// 	t_value	asleep;
-// 	t_value	spent;
-// 	t_value	left;
-
-
-// 	spent = 0;
-// 	asleep = get_time(MICROSECOND);
-// 	while (spent < sec && !dinner_finished(table))
-// 	{
-// 		spent = get_time(MICROSECOND) - asleep;
-// 		left = sec - spent;
-// 		if (left > MICROSECOND) usleep(left / 2);
-		
-// 		else 	while (sec > (get_time(MICROSECOND) - asleep) && !dinner_finished(table)); 
-// 	}
-
-
-// }
-
-
-
 
 void	ft_usleep(t_value sec, t_table *table)
 {
